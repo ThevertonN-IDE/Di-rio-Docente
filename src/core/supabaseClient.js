01acca04577b0c4d5306cb720cf749ed157a06b2
ThevertonN-IDE <thevertongutemberg15@gmail.com>
2026-09-14 17:32:53 -0300

    fix: corrige caminho relativo app.js e imports esm de supabase e dexie

--- src/core/supabaseClient.js
+++ src/core/supabaseClient.js
@@ -1,11 +1,7 @@
 // src/core/supabaseClient.js
-import { createClient } from '@supabase/supabase-js';
+import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm';
 
-const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL;
-const SUPABASE_ANON_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY;
-
-if (!SUPABASE_URL || !SUPABASE_ANON_KEY) {
-  throw new Error('Variáveis de ambiente do Supabase não configuradas no .env');
-}
+const SUPABASE_URL = 'https://nfwhkwouvxeyiwtliugs.supabase.co';
+const SUPABASE_ANON_KEY = 'sb_publishable_yeKYaByduIC5S53OPZjwgQ_koWBbFqE';
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
